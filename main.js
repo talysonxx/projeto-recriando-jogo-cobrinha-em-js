7dace314477aae99932011d02ed87df841246e4e
talysonxx <talysonoficial12@gmail.com>
2021-07-12 17:26:36 -0300

    movimento da cobrinha

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ let snake = []
 snake.push({
     x: 8 * box,
     y: 8 * box
-}) 
+})
+let direction = 'right'
 
 function criarBackground(){
     context.fillStyle = 'lightgreen'
@@ -18,6 +19,26 @@ function criarCobra(){
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
 }
+function iniciarJogo(){
+    criarBackground()
+    criarCobra()
 
-criarBackground()
-criarCobra()
+    let snakeX = snake[0].x
+    let snakeY = snak[0].y
+
+    if(direction == 'right') snakeX += box
+    if(direction == 'left') snakeX -= box
+    if(direction == 'up') snakeY += box
+    if(direction == 'down') snakeY -= box
+
+    snake.pop()
+
+    let novaCabeca = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(novaCabeca)
+}
+
+let jogo = setInterval(iniciarJogo(), 100)
